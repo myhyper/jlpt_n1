137e9f02f7914163ca6ea4016df3cbf7c4e47415
myhyper <myeongje@gmail.com>
2020-01-02 22:43:31 +0900

    Json file updated

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from gtts import gTTS
 import json
+import os
+import sys
+import random
+from playsound import playsound
+import threading
 
-def wget_mp3(word_list_file_name):
+day_count = 2 # 몇일째 단어장인가? 
+#original_data_file_name = "./words/day{:03d}.txt".format(day_count) # TXT
+original_data_file_name = "./words/day{:03d}.json".format(day_count) # JSON
+
+arr_words = json.load(open(original_data_file_name,'r')) # JSON
+arr_word_sounds = []
+def load_sound_files(word_list_file_name):
     f = open(word_list_file_name,'r')
     i = 0
     while True:
@@ -9,48 +20,57 @@ def wget_mp3(word_list_file_name):
         tmp = f.readline()
         if 0 < len(tmp):
             tmp = tmp.replace("\n",'')
-            arr.append(tmp)
+            arr_word_sounds.append(tmp)
             
-            file_name = "./words/mp3/002_{:03d}.mp3".format(i)
+            file_name = "./words/mp3/{:03d}_{:03d}.mp3".format(day_count, i)
             try:
                 open(file_name, 'r')
             except: # if not exists
-                print("Downloading mp3 ... ")
+                print("\t(Now downloading mp3 ... )")
                 a = gTTS(tmp,lang='ja')
                 a.save(file_name)
         else: break
     f.close()
 
+def bg_load_sound_files():
+    load_sound_files(original_data_file_name)
+threading.Thread(target=bg_load_sound_files, args=()).start()
 
-arr = []
-wget_mp3('./words/day002.txt')
 
-
-## Random Word Game
-import os
-import sys
-import random
-from playsound import playsound
-
-import threading
-
-def bg_run(word_idx):
-    file_name = "words/mp3/002_{:03d}.mp3".format(word_idx+1)
+def bg_play_mp3(word_idx):
+    file_name = "words/mp3/{:03d}_{:03d}.mp3".format(day_count, word_idx+1)
     playsound(file_name)
 
 
-idx = random.randint(0,len(arr)-1)
+## Random Word Game
+idx = random.randint(0,len(arr_words)-1) # JSON
 while True:
-    word = arr[idx]
-    print(word)
-    user_input = sys.stdin.readline()
-    if "?\n" == user_input:
-        threading.Thread(target=bg_run, args=([idx])).start()
-        pass
+    obj_word = arr_words[idx] # JSON
+    word = obj_word["word"]
+    
+    print("[Direction] Type the same sentence ( Commands =>  ? Pronunciation  / Furigana  q quit ) : ")
+    print("[Direction] This : ", word)
+    
+    # GET USER INPUT
+    user_input = sys.stdin.readline().replace("\n",'') # Trim
+    
+    # Compare it into the logic
+    if "?" == user_input:
+        threading.Thread(target=bg_play_mp3, args=([idx])).start() # Sound
+    elif "/" == user_input:
+        print("Hint : ", obj_word["ans"]) # JSON
+    elif "q" == user_input:
+        print("Hint : ", obj_word["ans"]) # JSON
     else:
-        if user_input.replace("\n",'') == word:
-            print("GOOD!")
-            idx = random.randint(0,len(arr)-1)
+        if user_input.replace(" ",'') == word.replace("\n",'').replace(" ",''):
+            print("[Direction] GOOD!")
+            obj_word["n_ok"] = str(int(obj_word["n_ok"])+1) # JSON
+            
+            # Next Word
+            idx = random.randint(0,len(arr_words)-1)
         else:
-            print("Wrong. Try again!")
-    print(user_input)
+            print("[Direction] Wrong. Try again!")
+            obj_word["n_ng"] = str(int(obj_word["n_ng"])+1) # JSON
+            
+        # Save the result
+        json.dump(arr_words, open(original_data_file_name,'w+'), indent=4) # JSON
