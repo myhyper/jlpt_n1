8fe75ac75b9f070f9691f3d4b4891bb5f067c993
myhyper <myeongje@gmail.com>
2020-01-03 13:53:51 +0900

    Sound Effect updated

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import random
 from playsound import playsound
 import threading
 
-day_count = 3 # How many days we've done? 
-level_name = "day" # N1
+#day_count = 3 # How many days we've done? 
+#level_name = "n1" # N1
 
-#day_count = 1 # How many days we've done? 
-#level_name = "n4_" # N4
+day_count = 1 # How many days we've done? 
+level_name = "n4" # N4
 
 # JSON
-# *.txt : just a note
-# *.json : [ {word, ans, idx, n_ok, n_ng} ] json list
-original_data_file_name = "./words/{}{:03d}.json".format(level_name,day_count) 
+# *.txt : make a list of word list manually
+# *.json : [ {word, ans, idx, n_ok, n_ng} ] a json list will be generated automatically
+original_data_file_name = "./words/{}_{:03d}.json".format(level_name,day_count) 
 
 arr_words = json.load(open(original_data_file_name,'r')) # JSON
 arr_word_sounds = []
@@ -29,7 +29,7 @@ def load_sound_files(word_json_file_name):
             tmp = tmp.replace("\n",'')
             arr_word_sounds.append(tmp)
             
-            file_name = "./words/mp3/{}{:03d}_{:03d}.mp3".format(level_name,day_count, i)
+            file_name = "./words/mp3/{}_{:03d}_{:03d}.mp3".format(level_name,day_count, i)
             try:
                 open(file_name, 'r')
             except: # if not exists
@@ -45,7 +45,7 @@ threading.Thread(target=bg_load_sound_files, args=()).start()
 
 
 def bg_play_mp3(word_idx):
-    file_name = "words/mp3/{}{:03d}_{:03d}.mp3".format(level_name,day_count, word_idx+1)
+    file_name = "words/mp3/{}_{:03d}_{:03d}.mp3".format(level_name,day_count, word_idx+1)
     playsound(file_name)
 
 
@@ -64,17 +64,19 @@ while True:
     # Compare it into the logic
     if "?" == user_input:   threading.Thread(target=bg_play_mp3, args=([idx])).start() # Sound
     elif "/" == user_input: print("Hint : ", obj_word["ans"]) # JSON
-    elif "q" == user_input: print("Hint : ", obj_word["ans"]) # JSON
+    elif "q" == user_input: print("Good bye!");break
     else:
         if user_input.replace(" ",'') == word.replace("\n",'').replace(" ",''):
+            playsound("./bgm/chime_up.wav")
             print("[Direction] GOOD!")
             obj_word["n_ok"] = str(int(obj_word["n_ok"])+1) # JSON
             
             # Next Word
             idx = random.randint(0,len(arr_words)-1)
         else:
+            playsound("./bgm/cough_x.wav")
             print("[Direction] Wrong. Try again!")
             obj_word["n_ng"] = str(int(obj_word["n_ng"])+1) # JSON
             
         # Save the result
-        json.dump(arr_words, open(original_data_file_name,'w+'), indent=4) # JSON
+        json.dump(arr_words, open(original_data_file_name,'w+'), indent=4, ensure_ascii=False) # JSON
